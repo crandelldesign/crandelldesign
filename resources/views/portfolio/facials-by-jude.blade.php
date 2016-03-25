ec2f1dfd1164b7010ebe659a065b98561e593617
Crandell, Matt <mcrandell@jrthompson.com>
2016-03-25 10:32:16 -0400

    Added url for Facials By Jude.

--- resources/views/portfolio/facials-by-jude.blade.php
+++ resources/views/portfolio/facials-by-jude.blade.php
@@ -27,6 +27,8 @@
                     <div class="col-sm-7 col-lg-8">
                         <p class="testimonial"><strong>&ldquo;</strong> Matt originally developed my website and I was very happy with everything he did. Working with Matt this time around, on updating my website to be mobile friendly, was another great experience. He was very professional and efficient. He always took time to address my concerns and was very patient when It took me a little time for decision making. I would highly recommend him. <strong>&rdquo;</strong></p>
                         <p class="testimonial-author text-right">&mdash; J. Cunningham, Owner</p>
+
+                        <p>See the full site at <a href="http://www.facialsbyjude.com" rel="external" target="_blank">http://www.facialsbyjude.com</a>.</p>
                         <!--<p>For the Clarkston Area Chamber of Commerce, I created not only a new logo, but also a full brand package for them. I featured the clock from the old logo: a landmark located downtown, updated the design with cleaner lines and an overall more modern look. I chose a new typeface to be used across the board. I used blue because it conveys reliability, while the orange is inviting and energetic. After discovering a multicolored wall in the chamber office, I used the tones from the wall to create all colors used in the logos.</p>
                         <p>All the logos keep a similar motif with varying levels in the words with the image of the logo on the left. This consistent look makes the logos instantly recognizable as a chamber event or activity.</p>
                         <p class="hidden-xs">Check out the additional logos below.</p>-->

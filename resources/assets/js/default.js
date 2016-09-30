9b749f001d2f4925473aa4eed3bad54e5e6730cb
Crandell, Matt <mcrandell@jrthompson.com>
2016-09-30 12:06:42 -0400

    Updated menu logic.

--- resources/assets/js/default.js
+++ resources/assets/js/default.js
@@ -1,25 +1,32 @@
 $(document).ready(function() {
+    /* Main Menu Logic */
     $("#main-menu").autoHidingNavbar();
+    $(".navbar-toggle").on("click", function () {
+        $(this).toggleClass("active");
+    });
+    $('#main-menu a').on('click', function(event) {
+        if($('.navbar-toggle').hasClass('active')) {
+            $('.navbar-toggle').removeClass('active');
+        }
+    });
     $('.animsition').animsition({
         inDuration: 300,
         outDuration: 300,
         linkElement: '.animsition-link',
     });
-    $(".navbar-toggle").on("click", function () {
-        $(this).toggleClass("active");
-    });
-    //jQuery for page scrolling feature - requires jQuery Easing plugin
+
+    /* jQuery for page scrolling feature - requires jQuery Easing plugin */
     $('a.page-scroll').bind('click', function(event) {
-        //var $anchor = $(this);
         var link = $(this).attr('href');
         var hash = link.substring(link.indexOf("#")+1);
         $('html, body').stop().animate({
-            //scrollTop: $($anchor.attr('href')).offset().top
             scrollTop: $('#'+hash).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-    setTimeout(function(){
+
+    /* Cancel Animsition if it's taking too long */
+    setTimeout(function() {
         $('.animsition').css({
             'animation-duration': '300ms',
             'opacity': '1'

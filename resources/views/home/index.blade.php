8cdd2ac970d9bcbcb5e3399e7f0ae113784c5fa1
Matt Crandell <mrcrandell@gmail.com>
2016-10-24 22:40:18 -0400

    2nd Test.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
 
 <div id="home" class="container-fluid page">
     <div class="content">
-        <h1 class="slogan heading">Test. Anybody can make a website, but only a few can make it look good!</h1>
+        <h1 class="slogan heading">Anybody can make a website, but only a few can make it look good!</h1>
         <div class="button-holder">
             <a href="#portfolio" class="btn btn-clear btn-lg page-scroll">See For Yourself <i class="fa fa-angle-down"></i></a>
         </div>

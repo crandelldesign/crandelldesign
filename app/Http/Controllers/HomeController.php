973870d1cc006f2d2eb14567e86f144fc1c967cd
Matt Crandell <mrcrandell@gmail.com>
2018-09-23 23:38:12 -0400

    Updated styling.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $view = view('home.index');
+        $view->title = "Crandell Design by Matt Crandell | Web Design and Development";
+        $view->description = "Web Design, web development, search engine optimization, and logo design by Matt Crandell servicing all of Metro Detroit.";
+
+        //$portfolio = $this->portfolio();
+        //$view->portfolio = $portfolio->take(8);
+
+        $view->active_page = 'home';
+
+        return $view;
+    }
+}
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+
+@section('content')
+<div id="home" class="container-fluid page">
+    <div class="content">
+        <h1 class="slogan heading">Anybody can make a website, but only a few can make it look good!</h1>
+        <div class="button-holder">
+            <a href="#portfolio" class="btn btn-clear btn-lg page-scroll">See For Yourself <i class="fa fa-angle-down"></i></a>
+        </div>
+    </div>
+</div>
+@endsection

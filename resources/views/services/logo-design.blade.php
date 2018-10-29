bb4837adf7a015ce13fcba6c593d94f3997ef2ec
Matt Crandell <mrcrandell@gmail.com>
2018-10-28 23:14:20 -0400

    Updated the word because of phone issues.

--- resources/views/services/logo-design.blade.php
+++ resources/views/services/logo-design.blade.php
@@ -17,7 +17,7 @@
             <div>
                 <h2>Logo Creation</h2>
                 <p>Do you have a company or organization that needs to establish brand presence? At Crandell Design, we can create a logo that will conform to the style you and your customers are looking for. Logos created by Crandell Design will be produced to fit any media you need.</p>
-                <h2>Logo Modification/Restoration</h2>
+                <h2>Logo Modification / Restoration</h2>
                 <p>Although you may already have a logo, it may not be up to par with what you may need or what you may require it to do. Crandell Design can update your logo to fit your modern needs.</p>
             </div>
         </div>
